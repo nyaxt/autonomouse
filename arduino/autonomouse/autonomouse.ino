d0205637184f8acadfa694e706202b8a748f89df
Philip Rogers <pdr@google.com>
2013-01-22 19:24:42 -0800

    Misc fixes for new hardware. WIP.

--- arduino/autonomouse/autonomouse.ino
+++ arduino/autonomouse/autonomouse.ino
@@ -3,7 +3,7 @@
 // TODO: scroll test isn't fully baked yet and needs work
 
 // pin settings
-const int photocellPin = 5; // analog pin
+const int photocellPin = 0; // analog pin
 
 // general test vars
 const unsigned long timeout = 1000000; // microseconds, test timeout
@@ -12,11 +12,12 @@ const unsigned long timeout = 1000000; // microseconds, test timeout
 // Note: RN42 chip needs to be put into mouse HID mode with "SH,0220".
 const byte mouseDown[] = {0xFD, 0x05, 0x02, 0x01, 0x00, 0x00, 0x00};
 const byte mouseUp[] = {0xFD, 0x05, 0x02, 0x00, 0x00, 0x00, 0x00};
-const byte mouseDownAndMoveDown[] = {0xFD, 0x05, 0x02, 0x01, 0x00, 0x04, 0x00};
+const byte mouseDownAndMoveDown[] = {0xFD, 0x05, 0x02, 0x01, 0x00, 0x7F, 0x00};
+const byte mouseDownAndMoveUp[] = {0xFD, 0x05, 0x02, 0x01, 0x00, 0x80, 0x00};
 const byte mouseMoveUp[] = {0xFD, 0x05, 0x02, 0x00, 0x00, 0xF0, 0x00};
-const byte mouseMoveDown[] = {0xFD, 0x05, 0x02, 0x00, 0x00, 0x10, 0x00};
+const byte mouseMoveDown[] = {0xFD, 0x05, 0x02, 0x00, 0x00, 0x0F, 0x00};
 const byte mouseMoveLeft[] = {0xFD, 0x05, 0x02, 0x00, 0xF0, 0x00, 0x00};
-const byte mouseMoveRight[] = {0xFD, 0x05, 0x02, 0x00, 0x10, 0x00, 0x00};
+const byte mouseMoveRight[] = {0xFD, 0x05, 0x02, 0x00, 0x0F, 0x00, 0x00};
 
 // commands for controlling the device via serial USB
 static const char CLICKTEST = 'a';
@@ -27,6 +28,8 @@ static const char MOVEMOUSEUP = '1';
 static const char MOVEMOUSEDOWN = '2';
 static const char MOVEMOUSELEFT = '3';
 static const char MOVEMOUSERIGHT = '4';
+static const char MOUSEDRAGUP = '5';
+static const char MOUSEDRAGDOWN = '6';
 
 // click test vars
 const int clickTestRuns = 150;
@@ -34,7 +37,7 @@ unsigned clickTimes[clickTestRuns];
 const int clickTestDelay = 503; // delay between tests. Make sure this won't sync on vsync! (aka not a multiple of 16.67ms)
 
 // scroll test vars
-const int scrollDelay = 19; // ms, time between mouse events
+const int scrollDelay = 60; // ms, time between mouse events
 const int scrollBufferSize = 150; // number of scroll events to record
 unsigned scrollTimes[scrollBufferSize];
 
@@ -45,7 +48,7 @@ static int PHOTO_IDK = 0;
 
 void setup(void) {
   Serial.begin(115200);
-  Serial1.begin(115200);
+  Serial2.begin(115200);
 }
 
 void loop(void) {
@@ -62,11 +65,11 @@ void loop(void) {
       char val = Serial.read();
       switch (val) {
         case (CLICKTEST):
-          delay(5000); // TESTING ONLY
+          delay(3000); // TESTING ONLY
           clickTest();
           return;
         case (SCROLLTEST):
-          delay(5000); // TESTING ONLY
+          delay(3000); // TESTING ONLY
           scrollTest();
           return;
         case (CALIBRATE):
@@ -79,6 +82,8 @@ void loop(void) {
         case (MOVEMOUSEDOWN): moveDown(); break;
         case (MOVEMOUSELEFT): moveLeft(); break;
         case (MOVEMOUSERIGHT): moveRight(); break;
+        case (MOUSEDRAGUP): clickAndMoveMouseUp(); break;
+        case (MOUSEDRAGDOWN): clickAndMoveMouseDown(); break;
         default:
           Serial.print("unknown command:");
           Serial.print(val);
@@ -146,36 +151,83 @@ void clickTest() {
   Serial.print("stddev: "); Serial.println(stddev);
 }
 
-
+// Perform scroll test
+// TODO(pdr): this is still hacky and not ready
+// TODO(pdr): values are hard-coded for a galaxy nexus and chrome
+int scrollableHeight = 3;
 void scrollTest() {
   Serial.println("Scroll Test");
 
   // Zero previous results
   for (int i = 0; i < scrollBufferSize; i++)
     scrollTimes[i] = 0;
+    
+  resetCursor();
 
   unsigned long startTime;
   int lastPhotoState = photoState();
   int scrollEventCount = 0;
   int mouseTimer = 0; // we can only fire mouse events so fast so this is used for limiting the rate.
   startTime = micros();
+  int changeCursorDirection = scrollableHeight;
+  bool cursorDirection = false;
   for (int time = 0; time < 15000; time++) {
     delay(1);
-    if (++mouseTimer % scrollDelay == 0)
-      clickAndMoveMouseDown();
+    if (++mouseTimer % scrollDelay == 0) {
+      if (cursorDirection)
+        clickAndMoveMouseUp();
+      else
+        clickAndMoveMouseDown();
+      changeCursorDirection--;
+    }
     int currentPhotoState = photoState();
     if (photoStateDifferent(currentPhotoState, lastPhotoState)) {
       lastPhotoState = currentPhotoState;
-      scrollTimes[scrollEventCount] = (micros() - startTime) / 1000;
+      scrollTimes[scrollEventCount] = (micros() - (startTime)) / 1000;
       if(++scrollEventCount >= scrollBufferSize)
         break;
 
       Serial.print("  "); Serial.print(scrollEventCount, DEC); Serial.print(": ");
       Serial.print(scrollTimes[scrollEventCount - 1], DEC); Serial.println("ms");
     }
+    if (changeCursorDirection == 0) {
+      cursorDirection = !cursorDirection;
+      changeCursorDirection = scrollableHeight;
+    }
   }
   releaseMouse();
   Serial.print("Event count: "); Serial.println(scrollEventCount, DEC);
+}
+
+// Move cursor to bottom-right
+// TODO(pdr): These values are hard-coded for a galaxy nexus and chrome
+void resetCursor() {
+  // move to bottom
+  for (int down = scrollableHeight + 50; down >= 0; down--) {
+    delay(scrollDelay);
+    moveDown();
+  }
+  // move to right
+  for (int right = 100; right >= 0; right--) {
+    delay(scrollDelay);
+    moveRight();
+  }
+  // move up, above menu bar
+  for (int up = 15; up >= 0; up--) {
+    delay(scrollDelay);
+    moveUp();
+  }
+  // move left one for padding
+  moveLeft();
+  delay(3000);
+}
+
+// Reposition cursor at bottom
+void repositionCursor() {
+  for (int down = scrollableHeight+3; down >= 0; down--) {
+    delay(scrollDelay);
+    moveDown();
+  }
 }
 
 // Show test results via USB.
@@ -213,8 +265,8 @@ void calibrate() {
 // adds hysteresis so noise doesn't cause us to bounce between on and off.
 int photoState() {
   int photoVoltage = analogRead(photocellPin);
-  if (photoVoltage >= 500) return PHOTO_ON;
-  if (photoVoltage <= 150) return PHOTO_OFF;
+  if (photoVoltage >= 100) return PHOTO_ON;
+  if (photoVoltage <= 50) return PHOTO_OFF;
   return PHOTO_IDK;
 }
 
@@ -225,25 +277,30 @@ boolean photoStateDifferent(int a, int b) {
 
 // Send mouse press command
 void clickMouse() {
-  Serial1.write(mouseDown, 7);
+  Serial2.write(mouseDown, 7);
 }
 
 // Send mouse depress command
 void releaseMouse() {
-  Serial1.write(mouseUp, 7);
+  Serial2.write(mouseUp, 7);
 }
 
 // Send mouse click and move down command
 void clickAndMoveMouseDown() {
-  Serial1.write(mouseDownAndMoveDown, 7);
+  Serial2.write(mouseDownAndMoveDown, 7);
+}
+
+// Send mouse click and move up command
+void clickAndMoveMouseUp() {
+  Serial2.write(mouseDownAndMoveUp, 7);
 }
 
 // Move mouse commands
 // useful for positioning the mouse over our click region.
-void moveDown() { Serial1.write(mouseMoveDown, 7); }
-void moveUp() { Serial1.write(mouseMoveUp, 7); }
-void moveLeft() { Serial1.write(mouseMoveLeft, 7); }
-void moveRight() { Serial1.write(mouseMoveRight, 7); }
+void moveDown() { Serial2.write(mouseMoveDown, 7); }
+void moveUp() { Serial2.write(mouseMoveUp, 7); }
+void moveLeft() { Serial2.write(mouseMoveLeft, 7); }
+void moveRight() { Serial2.write(mouseMoveRight, 7); }
 
 // Return true if the user input anything
 boolean thereIsUserInput() {
